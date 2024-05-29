4e269909debd9027f4e85cc07da2d4aff08a6edf
Anton Indyukov <indyukovanton@gmail.com>
2024-05-29 03:20:07 +0300

    update: bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,22 @@
 from telebot import *
+from telebot.types import Message
 from Parser import Parser_holidays
+import config
 
-def main():
-	token = ''
-
-	bot = TeleBot(token)
-	parser = Parser_holidays()
+bot = TeleBot(config.token)
+parser = Parser_holidays()
 
 
-	@bot.message_handler(commands=['start'])
-	def start(msg: types.Message):
-		bot.send_message(msg.from_user.id, 'Напишите команду /holidays, чтобы получить все текущие праздники.')
+@bot.message_handler(commands=['start'])
+def start_handler(message: Message):
+	bot.send_message(message.from_user.id, 'Напишите команду /holidays, чтобы получить все текущие праздники.')
 
 
-	@bot.message_handler(commands=['holidays'])
-	def pars_holiday_hander(msg: types.Message):
-		holidays = parser.get_holidays()
-		bot.send_message(msg.from_user.id, 'Текущий список праздников:\n'+'\n'.join(holidays))
+@bot.message_handler(commands=['holidays'])
+def pars_holiday_hander(message: Message):
+	holidays = parser.get_holidays()
+	bot.send_message(message.from_user.id, 'Текущий список праздников:\n'+'\n'.join(holidays))
 
 
+def run():
 	bot.infinity_polling()
-
-if __name__ == '__main__':
-	main()
